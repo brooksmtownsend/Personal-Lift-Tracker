159546d43b2c76382266390e50f9ca9d6c382cf5
Brooks Townsend <brooksmtownsend@gmail.com>
2017-02-01 12:55:51 -0500

    Initial Commit

---
+++ pplLiftTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  pplLiftTracker
+//
+//  Created by Brooks Townsend on 2/1/17.
+//  Copyright © 2017 Brooks Townsend. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
